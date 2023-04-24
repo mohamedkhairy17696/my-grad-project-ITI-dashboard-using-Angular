2a6810cd483ba4a0d25765ef7f32156a6b8359aa
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-04-24 11:23:59 +0200

    orders done

--- src/app/components/orders/orders.component.ts
+++ src/app/components/orders/orders.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { OrdersService } from 'src/app/services/orders.service';
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
-  styleUrls: ['./orders.component.css']
+  styleUrls: ['./orders.component.css'],
 })
 export class OrdersComponent {
+  userForm!: FormGroup;
+  userModel: any;
+  orderDetails: any;
 
+  constructor(private api: OrdersService, private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.getAllOrderDetails();
+  }
+
+  getAllOrderDetails() {
+    this.api.getAllOrders().subscribe(
+      (res) => {
+        this.orderDetails = res;
+        console.log(this.orderDetails);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  deleteOrderDetail(id: any) {
+    this.api.deleteOrder(id).subscribe(
+      (res) => {
+        alert('deleted successfully');
+        this.getAllOrderDetails();
+      },
+      (err) => {
+        alert('Failed to delete student information');
+      }
+    );
+  }
+
+  reset() {
+    this.userForm.reset();
+    this.userModel = {};
+  }
 }
---
+++ src/app/services/orders.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OrdersService {
+  constructor(private http: HttpClient) {}
+
+  baseUrl: string = environment.APIBaseURL;
+
+  postOrder(data: any) {
+    return this.http.post<any>(this.baseUrl + '/orders/dashboard', data);
+  }
+
+  getAllOrders() {
+    return this.http.get<any>(this.baseUrl + '/orders/dashboard');
+  }
+
+  deleteOrder(id: any) {
+    return this.http.delete<any>(this.baseUrl + '/orders/dashboard' + id);
+  }
+
+  updateOrder(data: any, id: number) {
+    return this.http.put<any>(this.baseUrl + '/orders/dashboard' + id, data);
+  }
+}
