aac59e81407b196e9060025130df1748080df783
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-04-29 11:59:03 +0300

    commit

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { OrdersService } from 'src/app/services/orders.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  userForm!: FormGroup;
+  userModel: any;
+  orderDetails: any;
 
+  constructor(private api: OrdersService, private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.getAllOrderDetails();
+  }
+
+  getAllOrderDetails() {
+    this.api.getAllOrders().subscribe(
+      (res) => {
+        this.orderDetails = res;
+        console.log(this.orderDetails);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
